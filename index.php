0a74809a201c9ac1fac3cbfb7044d608e2488b51
HyenaDesign <sam.hyenadesign@gmail.com>
2024-08-17 17:13:36 +0200

    Updated php template
    And created a stylesheet

--- index.php
+++ index.php
@@ -3,9 +3,39 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
-    <h1>TESTING</h1>
+    <header>
+        <div class="nav">
+            <h2>TODO APP</h2>
+            <a href="#"><h3>Edit</h3></a>
+        </div>
+    </header>
+    <section class="not-done">
+        <div class="nd-title">
+            <h3>Not Done</h3>
+        </div>
+        <div class="nd-tasks">
+            <ul>
+                <li><a href="#"><img src="#" alt="">Pay Rent</a></li>
+                <li><a href="#"><img src="#" alt="">Call Dad</a></li>
+            </ul>
+        </div>
+    </section>
+
+    <section class="done">
+        <div class="d-title">
+            <h3>Done</h3>
+        </div>
+        <div class="d-tasks">
+            <ul>
+                <li><a href="#"><img src="#" alt="">Book flight to London</a></li>
+                <li><a href="#"><img src="#" alt="">Go to the toilet</a></li>
+                <li><a href="#"><img src="#" alt="">Hydrate</a></li>
+            </ul>
+        </div>
+    </section>
 </body>
 </html>
